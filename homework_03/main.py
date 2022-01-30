27991d7380cbc04ce4095fa07686b78442c9f1c5
Evgeny <chapayevkrim@rambler.ru>
2022-01-30 21:23:45 +0300

    added homework_03

---
+++ homework_03/main.py
@@ -0,0 +1,20 @@
+# lesson 13 part 1 15:10
+# from fastapi import FastAPI
+#
+# app = FastAPI()
+#
+#
+# @app.get("/")
+# def read_root():
+#     return {"message": "Hello World"}
+
+# def main():
+#
+#     if __name__ == '__main__':
+#         uvicorn.run (
+#         "app: app",
+#         host = 'localhost',
+#         port = 8000,
+#         reload = True
+#         )
+#     main
