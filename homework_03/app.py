40a495377af018b482e69829c283e806971020ea
Evgeny <chapayevkrim@rambler.ru>
2022-02-07 07:53:39 +0300

    changed homework_03

--- homework_03/app.py
+++ homework_03/app.py
@@ -1,12 +1,8 @@
-# lesson 13 part 1 15:10
 from fastapi import FastAPI
-# from pythonping import ping
-
-app = FastAPI()
 
 
-# строка запуска
-# (venv) user@user-desktop:~/pythonProject/OTUSPython/homework_03$ uvicorn app:app --reload
+app = FastAPI() # You should consider
+
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
@@ -19,9 +15,3 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
     return {"item_id": item_id}
-
-# pong_waiter = await ws.ping()
-# await pong_waiter
-# # Я ничего не получил. После этого я также выполняю:
-#
-# pong_waiter.result()
