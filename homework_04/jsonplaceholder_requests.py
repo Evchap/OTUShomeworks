092ae650449e6337601cf1b1facc41ac460bfd5e
Evgeny <chapayevkrim@rambler.ru>
2022-03-08 21:39:03 +0300

    updated jsonplaceholder_requests.py

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -7,7 +7,6 @@ import aiohttp
 USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users"
 POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
 
-
 async def send_request(url):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
