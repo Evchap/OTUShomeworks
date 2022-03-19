9eff7910ea1a42ef69debbd51c56c9325aed9a09
Evgeny <chapayevkrim@rambler.ru>
2022-03-19 09:46:25 +0300

    updated homework_05

--- homework_05/app.py
+++ homework_05/app.py
@@ -1,6 +1,7 @@
 from flask import (
     Flask,
     render_template,
+    url_for,
 )
 
 app = Flask(__name__)
@@ -13,6 +14,10 @@ def index():
 @app.route("/about/")
 def about():
     return render_template('about.html', title="about")
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('about'))
 
 
 if __name__ == "__main__":
